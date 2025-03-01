a03ffbe6535936deadbba2d0853b3925737c8926
ajityaduv <100124329+ajityaduv@users.noreply.github.com>
2025-03-01 12:46:50 +0000

    2nd com

---
+++ Odd_even_Bitwise.java
@@ -0,0 +1,11 @@
+public class Odd_even_Bitwise{
+    public static String Odd_even(int n){
+        String status = n % 2 == 0 ? " is Even" : " is Odd";
+        return status;
+
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
